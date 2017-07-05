27d5b9f30816b6ade9532e1db894ec618697b090
Benny <benny@bdapps.net>
2017-07-05 17:28:12 +0300

    5.7.2017 - part 2

--- 2017.7.5/GesturesProject/GesturesProject/ViewController.swift
+++ 2017.7.5/GesturesProject/GesturesProject/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBAction func doubleTapAction(_ sender: UITapGestureRecognizer) {
+        sender.view?.backgroundColor = .randomColor
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let myView = self.view.viewWithTag(1) else{
+            return
+        }
+        
+        let radians = CGFloat.pi * 0.25 //45 degrees
+        myView.transform = CGAffineTransform(rotationAngle: radians)
+        
+        //back to default
+        //myView.transform = .identity
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +39,14 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
+
+
+
+
+
+
+
 
